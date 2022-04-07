355632458b89a6b1fd136e9c96f81187f2548a93
teunvanderploeg <29253@ma-web.nl>
2022-04-07 16:09:36 +0200

    Setup filament

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,26 @@
+const colors = require("tailwindcss/colors");
 module.exports = {
     content: [
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
         "./resources/**/*.vue",
+        "./vendor/filament/**/*.blade.php",
     ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: {
+                danger: colors.red,
+                primary: colors.pink,
+                success: colors.green,
+                warning: colors.yellow,
+                // white: colors.white,
+                // gray: colors.gray,
+            },
+        },
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+    ],
 }

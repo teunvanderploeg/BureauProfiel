797b4352f90c3308320e651b7826b8a371bb75ac
teunvanderploeg <29253@ma-web.nl>
2022-04-13 15:37:04 +0200

    Add questions migration

---
+++ database/migrations/2022_04_13_121528_create_questions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->string('question');
+            $table->string('slug')->unique();
+            $table->string('answer_type');
+            $table->text('sample_answers')->nullable();
+            $table->boolean('visible')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+};

dd248d84ac9a71c45e18a1449b1fe7d33ef58356
teunvanderploeg <29253@ma-web.nl>
2022-04-13 15:21:06 +0200

    Made master template

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>@yield('title')</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+</head>
+<body>
+<div class="'container mx-auto">
+    @yield('content')
+</div>
+</body>
+</html>

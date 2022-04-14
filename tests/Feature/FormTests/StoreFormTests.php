9b98b6c1b409ebef8fc6c52e63f11a453d4fe26b
teunvanderploeg <29253@ma-web.nl>
2022-04-14 12:14:45 +0200

    Add validation test

--- tests/Feature/FormTests/StoreFormTests.php
+++ tests/Feature/FormTests/StoreFormTests.php
@@ -30,4 +30,23 @@ class StoreFormTests extends TestCase
 
         $this->assertNotEmpty(Answer::all());
     }
+
+    public function it_gives_an_error_if_you_dont_send_all_the_data()
+    {
+        $emailQuestion = QuestionFactory::new()->create([
+            "question" => "Wat is je e-mail?",
+            "slug" => 'email',
+        ]);
+        $questions = QuestionFactory::times(3)->create();
+        $data = [];
+
+        foreach ($questions as $question) {
+            $data = $data + [$question->slug => 'Answer on ' . $question->question];
+        }
+
+        $this->post(route('form.store'), $data);
+
+        $this->errorError();
+        $this->assertEmpty(Answer::all());
+    }
 }

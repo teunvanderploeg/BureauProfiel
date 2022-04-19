8b0b707547072f45ea432f1a7b17b51ce0c95e80
teunvanderploeg <29253@ma-web.nl>
2022-04-19 08:34:14 +0200

    Add old values to input

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,17 +1,17 @@
 @switch($question->answer_type)
     @case('select')
-        <select name="{{ $question->slug }}" class="ml-auto">
-            @foreach($question->getArrayOfAnswers() as $answers)
-                <option value="{{$answers}}">{{$answers}}</option>
-            @endforeach
-        </select>
+    <select name="{{ $question->slug }}" class="ml-auto">
+        @foreach($question->getArrayOfAnswers() as $answers)
+            <option @if(old($question->slug) == $answers) selected @endif value="{{$answers}}">{{$answers}}</option>
+        @endforeach
+    </select>
     @break
 
     @case('checkbox')
-        <input type="checkbox" name="{{ $question->slug }}" class="ml-auto">
+    <input type="checkbox" @if(old($question->slug)) checked @endif name="{{ $question->slug }}" class="ml-auto">
     @break
 
     @default
-    <input type="{{ $question->answer_type }}" name="{{ $question->slug }}" class="ml-auto w-2/3">
-
+    <input type="{{ $question->answer_type }}" value="{{ old($question->slug) }}" name="{{ $question->slug }}"
+           class="ml-auto w-2/3">
 @endswitch

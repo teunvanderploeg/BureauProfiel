36bc9cb1a735c21a6cf568ee440bc7b161f370e8
teunvanderploeg <29253@ma-web.nl>
2022-04-21 10:49:24 +0200

    Add UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,21 +2,19 @@
 
 namespace Database\Seeders;
 
+use Database\Factories\UserFactory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call([
-            QuestionSeeder::class,
-            UserSeeder::class
-        ]);
+        UserFactory::new()->create(['email' => 'developer@bureauprofiel.com']);
     }
 }

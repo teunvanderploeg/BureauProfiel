fe5b4e39da6673eb336e414983e28dfde5fde4d9
teunvanderploeg <29253@ma-web.nl>
2022-04-29 11:39:37 +0200

    Add text model to dashboard

---
+++ app/Filament/Resources/TextResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TextResource\Pages;
+use App\Filament\Resources\TextResource\RelationManagers;
+use App\Models\Text;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+
+class TextResource extends Resource
+{
+    protected static ?string $model = Text::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('slug')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Textarea::make('text')
+                    ->required()
+                    ->maxLength(65535),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('slug'),
+                Tables\Columns\TextColumn::make('text'),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime(),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime(),
+            ])
+            ->filters([
+                //
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTexts::route('/'),
+            'create' => Pages\CreateText::route('/create'),
+            'edit' => Pages\EditText::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/TextResource/Pages/EditText.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\TextResource\Pages;
+
+use App\Filament\Resources\TextResource;
+use Filament\Resources\Pages\EditRecord;
+
+class EditText extends EditRecord
+{
+    protected static string $resource = TextResource::class;
+}

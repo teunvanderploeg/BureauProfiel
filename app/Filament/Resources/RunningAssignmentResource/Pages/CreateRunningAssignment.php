534f23fc3788e96a395cc1bc428aadbf93c3cf12
teunvanderploeg <29253@ma-web.nl>
2022-04-29 11:28:20 +0200

    Add RunningAssignment

---
+++ app/Filament/Resources/RunningAssignmentResource/Pages/CreateRunningAssignment.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\RunningAssignmentResource\Pages;
+
+use App\Filament\Resources\RunningAssignmentResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateRunningAssignment extends CreateRecord
+{
+    protected static string $resource = RunningAssignmentResource::class;
+}
---
+++ app/Filament/Resources/RunningAssignmentResource/Pages/ListRunningAssignments.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\RunningAssignmentResource\Pages;
+
+use App\Filament\Resources\RunningAssignmentResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListRunningAssignments extends ListRecords
+{
+    protected static string $resource = RunningAssignmentResource::class;
+}

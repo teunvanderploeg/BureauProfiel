99e477f54bfd45ab94bd40d990975a63619d7017
teunvanderploeg <29253@ma-web.nl>
2022-04-29 09:59:27 +0200

    fix name of Assignment

--- database/migrations/2022_04_28_135356_create_assignments_table.php
+++ database/migrations/2022_04_28_135356_create_assignments_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('assignments', function (Blueprint $table) {
             $table->id();
-            $table->integer('Assignment');
+            $table->integer('assignment');
             $table->string('day')->nullable();
             $table->date('date')->nullable();
             $table->dateTime('from');

9c597204bb7e89d4191be8c02d353784667d39a3
teunvanderploeg <29253@ma-web.nl>
2022-05-22 12:16:41 +0200

    Clean up database migration script

--- app/Console/Commands/MigrateDatabase.php
+++ app/Console/Commands/MigrateDatabase.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Models\Answer;
+use App\Models\Question;
 use App\Models\Respondent;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\File;
@@ -26,7 +27,7 @@ class MigrateDatabase extends Command
     /**
      * Execute the console command.
      *
-     * @return int
+     * @return int|void
      */
     public function handle()
     {
@@ -36,193 +37,82 @@ class MigrateDatabase extends Command
         foreach ($this->jsonDatabase as $tabel){
             if ($tabel->name == 'Respondenten'){
                 foreach ($tabel->data as $data){
-                    $voornaam = $data[1];
-                    $achternaam = $data[2];
-                    $adres = $data[3];
-                    $postcode = $data[4];
-                    $plaats = $data[5];
-                    $telefoonPrive = $data[6];
-                    $telefoonWerk = $data[7];
-                    $geslacht = $data[8];
-                    $geboorteDatum = $data[9];
-                    $burgelijkestaat = $data[10];
-                    $beroep = $data[11];
-                    $opleiding = $data[12];
-                    $gezinssamenstelling = $data[13];
-                    $kinderen = $data[14]; // maak een function die de kinderen pakt
-                    $info = $data[15];
-                    $huisdieren = $data[16];
-                    $auto = $data[17];
-                    $sigaret = $data[18];
-                    $rokenMerk = $data[19];
-                    $shag = $data[20];
-                    $shagMerk = $data[21];
-                    $sigaar = $data[22];
-                    $sigaarMerk = $data[23];
-                    $rokenSoort = $data[24];
-                    $telefoonMobiel = $data[25];
+
                     $email = $data[26];
-                    $vaatwasser = $data[27];
-                    $landVanHerkomst = $data[28];
-                    if(empty($email)){
-                        return;
-                    }
-                    $respondent = Respondent::query()->create([
+                    if(empty($email)) return;
+
+                    $this->respondent = Respondent::query()->create([
                         'email' => $email,
-                        'notes' => $info,
+                        'notes' => $info = $data[15],
                         'accepted' => true,
                     ]);
 
-                    Answer::query()->create([
-                        'respondent_id' => $respondent->id,
-                        'question_id' => 1,
-                        'answer' => $voornaam,
-                    ]);
-                    Answer::query()->create([
-                        'respondent_id' => $respondent->id,
-                        'question_id' => 2,
-                        'answer' => $achternaam,
-                    ]);
-                    Answer::query()->create([
-                        'respondent_id' => $respondent->id,
-                        'question_id' => 3,
-                        'answer' => $adres,
-                    ]);
-                    Answer::query()->create([
-                        'respondent_id' => $respondent->id,
-                        'question_id' => 4,
-                        'answer' => $postcode,
-                    ]);
-                    Answer::query()->create([
-                        'respondent_id' => $respondent->id,
-                        'question_id' => 5,
-                        'answer' => $plaats,
-                    ]);
-                    Answer::query()->create([
-                        'respondent_id' => $respondent->id,
-                        'question_id' => 6,
-                        'answer' => $telefoonPrive ?? $telefoonMobiel ?? $telefoonWerk,
-                    ]);
-                    Answer::query()->create([
-                        'respondent_id' => $respondent->id,
-                        'question_id' => 7,
-                        'answer' => $email,
-                    ]);
-                    Answer::query()->create([
-                        'respondent_id' => $respondent->id,
-                        'question_id' => 8,
-                        'answer' => $geslacht,
-                    ]);
-                    Answer::query()->create([
-                        'respondent_id' => $respondent->id,
-                        'question_id' => 9,
-                        'answer' => $geboorteDatum,
-                    ]);
-                    Answer::query()->create([
-                        'respondent_id' => $respondent->id,
-                        'question_id' => 11,
-                        'answer' => isset($kinderen),
-                    ]);
-                    Answer::query()->create([
-                        'respondent_id' => $respondent->id,
-                        'question_id' => 12,
-                        'answer' => $this->getChildDate($data[0], ),
-                    ]);
-                    Answer::query()->create([
-                        'respondent_id' => $respondent->id,
-                        'question_id' => 16,
-                        'answer' => $opleiding,
-                    ]);
-                    Answer::query()->create([
-                        'respondent_id' => $respondent->id,
-                        'question_id' => 31,
-                        'answer' => true,
-                    ]);
-                    Answer::query()->create([
-                        'respondent_id' => $respondent->id,
-                        'question_id' => 32,
-                        'answer' => $landVanHerkomst,
-                    ]);
-                    Answer::query()->create([
-                        'respondent_id' => $respondent->id,
-                        'question_id' => 33,
-                        'answer' => $sigaret,
-                    ]);
-                    Answer::query()->create([
-                        'respondent_id' => $respondent->id,
-                        'question_id' => 34,
-                        'answer' => $rokenMerk,
-                    ]);
-                    Answer::query()->create([
-                        'respondent_id' => $respondent->id,
-                        'question_id' => 35,
-                        'answer' => $shag,
-                    ]);
-                    Answer::query()->create([
-                        'respondent_id' => $respondent->id,
-                        'question_id' => 36,
-                        'answer' => $shagMerk,
-                    ]);
-                    Answer::query()->create([
-                        'respondent_id' => $respondent->id,
-                        'question_id' => 37,
-                        'answer' => $sigaar,
-                    ]);
-                    Answer::query()->create([
-                        'respondent_id' => $respondent->id,
-                        'question_id' => 38,
-                        'answer' => $sigaarMerk,
-                    ]);
-                    Answer::query()->create([
-                        'respondent_id' => $respondent->id,
-                        'question_id' => 39,
-                        'answer' => $rokenSoort,
-                    ]);
-                    Answer::query()->create([
-                        'respondent_id' => $respondent->id,
-                        'question_id' => 22,
-                        'answer' => $beroep,
-                    ]);
-                    Answer::query()->create([
-                        'respondent_id' => $respondent->id,
-                        'question_id' => 40,
-                        'answer' => $huisdieren,
-                    ]);
-                    Answer::query()->create([
-                        'respondent_id' => $respondent->id,
-                        'question_id' => 41,
-                        'answer' => $auto,
-                    ]);
-                    Answer::query()->create([
-                        'respondent_id' => $respondent->id,
-                        'question_id' => 42,
-                        'answer' => $vaatwasser,
-                    ]);
-                    Answer::query()->create([
-                        'respondent_id' => $respondent->id,
-                        'question_id' => 43,
-                        'answer' => $gezinssamenstelling,
-                    ]);
-                    Answer::query()->create([
-                        'respondent_id' => $respondent->id,
-                        'question_id' => 44,
-                        'answer' => $burgelijkestaat,
-                    ]);
+                    $this->makeAnswer('voornaam', $voornaam = $data[1]);
+                    $this->makeAnswer('achternaam', $achternaam = $data[2]);
+                    $this->makeAnswer('adres', $adres = $data[3]);
+                    $this->makeAnswer('postcode', $postcode = $data[4]);
+                    $this->makeAnswer('woonplaats', $plaats = $data[5]);
+                    $this->makeAnswer('telefoonnummer', $telefoonPrive = $data[6] ?? $telefoonMobiel = $data[25] ?? $telefoonWerk = $data[7]);
+                    $this->makeAnswer('email', $email);
+                    $this->makeAnswer('geslacht', $geslacht = $data[8]);
+                    $this->makeAnswer('geboortedatum', $geboorteDatum = $data[9]);
+                    $this->makeAnswer('thuiswonende-kinderen', isset($data[14]));
+                    $this->makeAnswer('geboortedatum-kind-1', $this->getChild($data[0], 1));
+                    $this->makeAnswer('geslacht-kind-1', $this->getChild($data[0], 1, True));
+                    $this->makeAnswer('geboortedatum-kind-2', $this->getChild($data[0], 2));
+                    $this->makeAnswer('geslacht-kind-2', $this->getChild($data[0], 2, True));
+                    $this->makeAnswer('geboortedatum-kind-3', $this->getChild($data[0], 3));
+                    $this->makeAnswer('geslacht-kind-3', $this->getChild($data[0], 3, True));
+                    $this->makeAnswer('geboortedatum-kind-4', $this->getChild($data[0], 4));
+                    $this->makeAnswer('geslacht-kind-4', $this->getChild($data[0], 4, True));
+                    $this->makeAnswer('afgeronde-opleidingsniveau', $opleiding = $data[12]);
+                    $this->makeAnswer('Privacy-Statement', True);
+                    $this->makeAnswer('land-van-herkomst', $landVanHerkomst = $data[28]);
+                    $this->makeAnswer('roken-soort', $sigaret = $data[18]);
+                    $this->makeAnswer('roken-merk', $rokenMerk = $data[19]);
+                    $this->makeAnswer('shag', $shag = $data[20]);
+                    $this->makeAnswer('shag-merk', $shagMerk = $data[21]);
+                    $this->makeAnswer('sigaar', $sigaar = $data[22]);
+                    $this->makeAnswer('sigaar-merk', $sigaarMerk = $data[23]);
+                    $this->makeAnswer('sigaret', $rokenSoort = $data[24]);
+                    $this->makeAnswer('werkzaam-functie', $beroep = $data[11]);
+                    $this->makeAnswer('huisdier', $huisdieren = $data[16]);
+                    $this->makeAnswer('auto', $auto = $data[17]);
+                    $this->makeAnswer('vaatwasser', $vaatwasser = $data[27]);
+                    $this->makeAnswer('gezinssamenstelling', $gezinssamenstelling = $data[13]);
+                    $this->makeAnswer('burgelijkestaat', $burgelijkestaat = $data[10]);
                 }
             }
         }
     }
 
-    public function getChildDate($respondentId)
+    public function makeAnswer($questionSlug, $answer)
     {
+        Answer::query()->create([
+            'respondent_id' => $this->respondent->id,
+            'question_id' => Question::query()->firstWhere('slug', '=', $questionSlug)->first('id')->id,
+            'answer' => $answer,
+        ]);
+    }
+
+    public function getChild($respondentId, $child, $gender = False)
+    {
+        $childCount = 0;
         foreach ($this->jsonDatabase as $tabel){
             if ($tabel->name == 'Kinderen'){
                 foreach ($tabel->data as $data){
-                    if($data[1] == $respondentId){
-                        return $data[3];
+                    if($data[0] == $respondentId){
+                        $childCount++;
+                        if ($childCount == $child){
+                            if ($gender){
+                                return $data[4];
+                            }
+                            return $data[3];
+                        }
                     }
                 }
             }
         }
+        return null;
     }
 }

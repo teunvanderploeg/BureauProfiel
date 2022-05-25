7bcbd7a07ad0319d657a2596a7133c41e3fe7692
teunvanderploeg <29253@ma-web.nl>
2022-05-25 14:56:51 +0200

    Change the colors

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <div class="bg-white py-32 px-10 min-h-screen ">
-        <form class="bg-gray-100 p-10 w-full md:w-5/6 lg:w-3/4 mx-auto" method="POST"
+        <form class="bg-bp_white rounded-2xl p-10 w-full md:w-5/6 lg:w-3/4 mx-auto" method="POST"
               action="{{ route('form.store') }}">
             @if(session()->has('message'))
                 <div class="text-green-500 font-bold mx-auto">
@@ -28,7 +28,7 @@
             @endforeach
             <div class="text-right">
                 <input type="submit" value="Send"
-                       class="py-3 rounded-2xl px-8 bg-blue-500 text-white font-bold cursor-pointer">
+                       class="text-white font-bold whitespace-nowrap cursor-pointer px-7 py-2 rounded-r-2xl transition border border-4 hover:text-black hover:bg-bp_white rounded-b-2xl bg-bp_purple border-bp_purple">
             </div>
         </form>
     </div>
